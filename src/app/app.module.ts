e1427e4c7aebf4db992d521c0e3803c35fa6e7a6
supermyle <kylemanley99@gmail.com>
2022-10-17 07:30:41 -0400

    Working table with Paginator controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +18,8 @@ import { SensordataTableComponent } from './ui-components/sensordata-table/senso
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ui-components/sensordata-table/sensordata-table.component.ts
+++ src/app/ui-components/sensordata-table/sensordata-table.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SensorRecord } from '../../models/sensor-record.module';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import sensorData from '../../../assets/sensordata.json';
 
-const SENSOR_DATA: SensorRecord[] = sensorData;
 
 @Component({
   selector: 'app-sensordata-table',
   templateUrl: './sensordata-table.component.html',
   styleUrls: ['./sensordata-table.component.css']
 })
-export class SensordataTableComponent implements OnInit {
+export class SensordataTableComponent {
+  @ViewChild('paginator') paginator!: MatPaginator;
   displayedColumns: string[] = ['Date and Time', 'Temperature', 'Humidity', 'Voltage', 'Input On'];
-  dataSource = SENSOR_DATA;
+  SENSOR_DATA: SensorRecord[] = sensorData;
+  dataSource!: MatTableDataSource<SensorRecord>;
+  pageSizes = [5,10,15];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource(this.SENSOR_DATA);
+    this.dataSource.paginator = this.paginator;
   }
 }
